a6664c7ef05f004dae792920fc388d83dfe4f9aa
arjona55 <alexarjona77@gmail.com>
2024-05-20 17:35:22 +0200

    db2

--- moduls.py
+++ moduls.py
@@ -3,44 +3,56 @@ from sqlalchemy import ForeignKey
 db = SQLAlchemy()
 
 
-class user(db.Model):
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
     username = db.Column(db.String(80), primary_key=True)
     password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    carrito = db.relationship('carrito', backref='user', lazy=True)
-    carrito = db.relationship('pedido', backref='user', lazy=True)
+    carritos = db.relationship('Carrito', backref='user', lazy=True)
+    pedidos = db.relationship('Pedido', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
-class carrito(db.Model):
+class Producto(db.Model):
+    __tablename__ = 'producto'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), ForeignKey('user.username'), primart_key=True)
-    producto = db.Column(db.String(80), ForeignKey('producto.nombre'), primary=True)
+    nombre = db.Column(db.String(80), nullable=False, unique=True)
+    precio = db.Column(db.Float(10, 2), nullable=False)
+    carritos = db.relationship('Carrito', backref='producto', lazy=True)
+    pedidos = db.relationship('Pedido', backref='producto', lazy=True)
+
+    def __repr__(self):
+        return '<Producto %r>' % self.nombre
+
+class Carrito(db.Model):
+    __tablename__ = 'carrito'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), db.ForeignKey('user.username'), primary_key=True)
+    producto_nombre = db.Column(db.String(80), db.ForeignKey('producto.nombre'), primary_key=True)
     cantidad = db.Column(db.Integer, nullable=False)
     total = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return '<Carrito %r>' % self.username
+        return '<Carrito %r, %r, %r>' % (self.id, self.username, self.producto_nombre)
 
-class producto(db.Model):
+class Pedido(db.Model):
+    __tablename__ = 'pedido'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(80), nullable=False)
-    precio = db.Column(db.Float(10,2), nullable=False)
-    carrito = db.relationship('carrito', backref='producto', lazy=True)
-    carrito = db.relationship('pedido', backref='producto', lazy=True)
-
-    def __repr__(self):
-        return '<Producto %r>' % self.username
-
-class pedido(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), ForeignKey('user.username'))
-    producto = db.Column(db.String(80), ForeignKey('producto.nombre'))
+    username = db.Column(db.String(80), db.ForeignKey('user.username'))
+    producto_nombre = db.Column(db.String(80), db.ForeignKey('producto.nombre'))
     cantidad = db.Column(db.Integer, nullable=False)
     total = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return '<Pedido %r>' % self.username
+        return '<Pedido %r>' % self.id
+
     
     
