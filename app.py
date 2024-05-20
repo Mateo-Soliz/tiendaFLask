23749adcffc590f7a1c3f6749fc7e2ad1167df00
Mateo Daniel Soliz Rueda <mdsolizr@alumnes.santjosepobrer.net>
2024-05-20 17:41:35 +0200

    asd

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask,render_template,redirect,url_for
+from moduls import db,User,Producto,Carrito,Pedido
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 
 @app.route('/')
 def index():
+    javier = User(username = 'javier', password = '1234', email = 'javier@gmail.com')
+    db.session.add(javier)
+    db.session.commit()
     return render_template('index.html')
 
 @app.route('/<string:username>/carrito')
@@ -11,4 +19,6 @@ def carrito(username):
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
